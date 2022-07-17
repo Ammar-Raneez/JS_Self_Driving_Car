ae9515d7377afbb4759712316575b464414c7ee0
ammar <ammarraneez@gmail.com>
2022-07-17 22:06:04 +0530

    Add simple canvas movement

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 const canvas = document.getElementById('car-canvas');
-canvas.height = window.innerHeight;
 canvas.width = 200;
 
 // Ref to canvas context
 const ctx = canvas.getContext('2d');
+
+const car = new Car(100, 100, 30, 30);
+car.draw(ctx);
+
+animate();
+
+function animate() {
+  car.update();
+  canvas.height = window.innerHeight;
+  car.draw(ctx);
+
+  // Calls animate again and again many times per second
+  requestAnimationFrame(animate);
+}
