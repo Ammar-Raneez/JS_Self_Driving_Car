aa0d71729fd85e2f0edddb42290b16332be94a22
ammar <ammarraneez@gmail.com>
2022-07-19 09:56:24 +0530

    feat: create road design

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ canvas.width = 200;
 
 // Ref to canvas context
 const ctx = canvas.getContext('2d');
-
-const car = new Car(100, 100, 30, 30);
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 30);
 car.draw(ctx);
 
 animate();
@@ -12,6 +12,7 @@ animate();
 function animate() {
   car.update();
   canvas.height = window.innerHeight;
+  road.draw(ctx);
   car.draw(ctx);
 
   // Calls animate again and again many times per second
