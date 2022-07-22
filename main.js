f11e08b5ef94e1b26c239a8f3a9786e422d919d9
ammar <ammarraneez@gmail.com>
2022-07-22 11:16:34 +0530

    feat: connect car with neural network

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ canvas.width = 200;
 // Ref to canvas context
 const ctx = canvas.getContext('2d');
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 30, 'KEYS');
+const car = new Car(road.getLaneCenter(1), 100, 30, 30, 'AI');
 const traffic = [
   new Car(road.getLaneCenter(1), -100, 30, 50, 'DUMMY', 2),
 ];
